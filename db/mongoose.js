f87a9a6b2d8b8fb3a5b746dfeba70f17f5a43a81
Khai <Khai@tttkhai93@gmail.com>
2018-03-16 00:36:17 -0700

    commit

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose');
-var key = require('../config/key');
+var key = require('../config/dev');
 var url = key.mongoURI;
 var localMongoose = 'mongodb://localhost:27017/survey-app';
 mongoose.connect(url || localMongoose);
